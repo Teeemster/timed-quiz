a8ce166e7d27b22e9b2c0cedb2ea0f9dcfd42453
Teeemster <jonathan_beach@ymail.com>
2021-10-03 14:45:22 -0700

    update to questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,6 +5,10 @@ var timeLeft = 100
 var timerEl = document.getElementById('countdown');
 var wrongSubtract = document.querySelector('.Wrong');
 
+//Start ID
+var startButton = document.getElementById('Start')
+var welcomeInfo = document.getElementById('Welcome')
+
 //Correct Answer ID's
 var correctOne = document.getElementById('Correct1')
 var correctTwo = document.getElementById('Correct2')
@@ -13,14 +17,21 @@ var correctFour = document.getElementById('Correct4')
 var correctFive = document.getElementById('Correct5')
 
 //Question ID's
-var questionOne = document.getElementById(Question1)
-var questionTwo = document.getElementById(Question2)
-var questionThree = document.getElementById(Question3)
-var questionFour = document.getElementById(Question4)
-var questionFive = document.getElementById(Question5)
+var questionOne = document.getElementById('Question1')
+var questionTwo = document.getElementById('Question2')
+var questionThree = document.getElementById('Question3')
+var questionFour = document.getElementById('Question4')
+var questionFive = document.getElementById('Question5')
 
 
 //ALL FUNCTIONS
+
+//Show Question 1
+startButton.addEventListener('click', function () {
+    questionOne.style.display = "block";
+    welcomeInfo.style.display = "none";
+}
+)
 
 //Show Question 2
 correctOne.addEventListener('click', function () {
