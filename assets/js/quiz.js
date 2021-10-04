646ec2c07a58e59c94cc9028817497f6165a299f
Teeemster <jonathan_beach@ymail.com>
2021-10-03 20:30:53 -0700

    quiz updates

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,26 +3,32 @@
 //Timer Vars
 var timeLeft = 100
 var timerEl = document.getElementById('countdown');
-var wrongSubtract = document.querySelector('.Wrong');
 
 //Start ID
-var startButton = document.getElementById('Start')
-var welcomeInfo = document.getElementById('Welcome')
+var startButton = document.getElementById('Start');
+var welcomeInfo = document.getElementById('Welcome');
 
 //Correct Answer ID's
-var correctOne = document.getElementById('Correct1')
-var correctTwo = document.getElementById('Correct2')
-var correctThree = document.getElementById('Correct3')
-var correctFour = document.getElementById('Correct4')
-var correctFive = document.getElementById('Correct5')
+var correctOne = document.getElementById('Correct1');
+var correctTwo = document.getElementById('Correct2');
+var correctThree = document.getElementById('Correct3');
+var correctFour = document.getElementById('Correct4');
+var correctFive = document.getElementById('Correct5');
+
+//Wrong Answer Class
+var wrongSubtract = document.getElementsByClassName('Wrong');
 
 //Question ID's
-var questionOne = document.getElementById('Question1')
-var questionTwo = document.getElementById('Question2')
-var questionThree = document.getElementById('Question3')
-var questionFour = document.getElementById('Question4')
-var questionFive = document.getElementById('Question5')
+var questionOne = document.getElementById('Question1');
+var questionTwo = document.getElementById('Question2');
+var questionThree = document.getElementById('Question3');
+var questionFour = document.getElementById('Question4');
+var questionFive = document.getElementById('Question5');
 
+//High Score ID's
+var finalScore = document.getElementById('FinalScore')
+var highScore = document.getElementById('HighScore')
+var highScoreButton = document.getElementById('HighScoreButton')
 
 //ALL FUNCTIONS
 
@@ -30,6 +36,7 @@ var questionFive = document.getElementById('Question5')
 startButton.addEventListener('click', function () {
     questionOne.style.display = "block";
     welcomeInfo.style.display = "none";
+    countdown();
 }
 )
 
@@ -78,9 +85,21 @@ function countdown() {
         , 1000);
 }
 
-countdown();
+//Show Final Score
+correctFive.addEventListener('click', function () {
+    finalScore.style.display = "block";
+    questionFive.style.display = "none";
+    clearInterval(timeInterval)
+}
+)
 
-//Wrong Answer Deduction
-wrongSubtract.addEventListener('click', () => {
-    timeLeft = timeLeft - 10
-});
+//Show High Score
+highScoreButton.addEventListener('click', function () {
+    highScore.style.display = "block";
+    finalScore.style.display = "none";
+}
+)
+
+
+//Winning Scores
+localStorage.setItem('timeLeft', JSON.stringify([timeLeft]));
